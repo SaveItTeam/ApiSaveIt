3ded9309ed6c6553c253b7df4e5f20894a4ec15c
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-07-25 12:51:14 -0300

    Subindo alguns métodos de CRUD do EnderecoService

--- src/main/java/com/api/Service/EnderecoService.java
+++ src/main/java/com/api/Service/EnderecoService.java
@@ -1,8 +1,12 @@
 package com.api.Service;
 
+import com.api.Model.Endereco;
 import com.api.Repository.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class EnderecoService {
@@ -13,6 +17,37 @@ public class EnderecoService {
         this.enderecoRepository = enderecoRepository;
     }
 
+
+    //    Métodos de busca
+    public List<Endereco> listarEnderecos(){return enderecoRepository.findAll();}
+
+    // Inserção de enderecos
+    public Endereco inserirEndereco(Endereco endereco) {
+        return enderecoRepository.save(endereco);
+    }
+
+    // Deleção de endereços
+    public void excluirProduto(Long id) {
+        // Se o produto não for encontrado, pode ser lançado um erro posteriormente.
+        // Dependendo da implementação do repository, pode ser necessário buscar primeiro o produto.
+        //        Endereco existe = buscarProdutoPorId(id);
+        enderecoRepository.deleteById(id);
+        //        return;
+    }
+    // Atualização de endereços
+    public Endereco atualizarEndereco(Long id, Endereco enderecoAtualizado) {
+        Endereco endereco = enderecoRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Endereco com ID " + id + " não encontrado"));
+
+        endereco.setNome(enderecoAtualizado.getNome());
+        endereco.setEstado(enderecoAtualizado.getEstado());
+        endereco.setRua(enderecoAtualizado.getRua());
+        endereco.setCep(enderecoAtualizado.getCep());
+        endereco.setBairro(enderecoAtualizado.getBairro());
+        endereco.setComplemento(enderecoAtualizado.getComplemento());
+
+        return enderecoRepository.save(endereco);
+    }
 
 
 }
