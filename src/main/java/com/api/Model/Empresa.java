6fe774ea0e2b6ce33be77fe9e0eb797539985d07
nathanfuchida <nathan.barreto@germinare.org.br>
2025-07-25 16:27:39 -0300

    Subindo model de empresa pronta

--- src/main/java/com/api/Model/Empresa.java
+++ src/main/java/com/api/Model/Empresa.java
@@ -1,4 +1,111 @@
 package com.api.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Empresa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull(message = "Campo vazio")
+    @Column(unique = true)
+    private long id;
+    @Column(length = 20)
+    private String cnpj;
+    @Column(length = 100)
+    private String nome;
+    @Column(length = 50)
+    private String codigo;
+    @Column(length = 100)
+    private String email;
+    @Column(length = 50)
+    private String tipoUsuario;
+    @Column(length = 20)
+    private String telefone;
+
+    public Empresa() {
+    }
+
+    public Empresa(long id, String telefone, String email, String tipoUsuario, String codigo, String nome, String cnpj) {
+        this.id = id;
+        this.telefone = telefone;
+        this.email = email;
+        this.tipoUsuario = tipoUsuario;
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "id=" + id +
+                ", cnpj='" + cnpj + '\'' +
+                ", nome='" + nome + '\'' +
+                ", codigo='" + codigo + '\'' +
+                ", email='" + email + '\'' +
+                ", tipoUsuario='" + tipoUsuario + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+
+
+
 }
