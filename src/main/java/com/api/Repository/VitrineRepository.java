6b7e569a95863d4ff20244804c9c7bcef3e122c9
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-08-14 16:21:26 -0300

    Subindo os arquivos de Vitrine

--- src/main/java/com/api/Repository/VitrineRepository.java
+++ src/main/java/com/api/Repository/VitrineRepository.java
@@ -1,4 +1,8 @@
 package com.api.Repository;
 
-public interface VitrineRepository {
+import com.api.Model.Produto;
+import com.api.Model.Vitrine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VitrineRepository extends JpaRepository<Vitrine, Long> {
 }
