cb540bfccca4b51147a3e42e91e93f662ae0b9bc
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-09-25 08:26:45 -0300

    Subindo employee para swagger

--- src/main/java/com/api/dto/employee/EmployeeRequestDTO.java
+++ src/main/java/com/api/dto/employee/EmployeeRequestDTO.java
@@ -1,20 +1,25 @@
 package com.api.dto.employee;
 
 import com.api.Model.Employee;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
 
 public class EmployeeRequestDTO {
     @NotNull(message = "Nome vazio")
     @Column(length = 255)
+    @Schema(description = "Nome do funcionário", example = "João Silva")
     private String name;
     @Column(length = 255)
     @NotNull(message = "Email vazio")
+    @Schema(description = "email do funcionario", example = "exemplo@gmail.com")
     private String email;
     @NotNull(message = "Senha vazia")
+    @Schema(description = "Senha do funcionário", example = "senha123")
     @Column(length = 255)
     private String password;
     @NotNull(message = "empresa_id vazio")
+    @Schema(description = "ID da empresa do funcionário", example = "1")
     private long enterprise_id;
 
     public EmployeeRequestDTO() {
--- src/main/java/com/api/dto/employee/EmployeeResponseDTO.java
+++ src/main/java/com/api/dto/employee/EmployeeResponseDTO.java
@@ -1,13 +1,18 @@
 package com.api.dto.employee;
 
 import com.api.Model.Employee;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
 
 public class EmployeeResponseDTO {
+    @Schema(description = "ID do funcionário", example = "1")
     private long id;
+    @Schema(description = "Nome do funcionário", example = "João Silva")
     private String name;
+    @Schema(description = "email do funcionario", example = "exemplo@gmail.com")
     private String email;
+    @Schema(description = "ID da empresa do funcionário", example = "1")
     private long enterprise_id;
 
     public EmployeeResponseDTO(long enterprise_id, String email, String name, long id) {
