fa0176edc1bb99ce42d4fab35c6fef00e4b572c7
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-09-25 08:28:38 -0300

    Subindo showcase para swagger

--- src/main/java/com/api/dto/showcase/ShowcaseRequestDTO.java
+++ src/main/java/com/api/dto/showcase/ShowcaseRequestDTO.java
@@ -1,19 +1,24 @@
 package com.api.dto.showcase;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
 
 public class ShowcaseRequestDTO {
     @Column(unique = true)
+    @Schema(description = "ID da vitrine", example = "1")
     private long id;
 
     @Column(length = 500)
+    @Schema(description = "Descrição do produto na vitrine", example = "Produto em promoção")
     private String description;
 
     @NotNull(message = "Preço vazio")
+    @Schema(description = "Preço do produto na vitrine", example = "99.99")
     private double price;
 
     @NotNull(message = "lote_id vazio")
+    @Schema(description = "ID do lote associado à vitrine", example = "1")
     private long batch_id;
 
     public ShowcaseRequestDTO() {
