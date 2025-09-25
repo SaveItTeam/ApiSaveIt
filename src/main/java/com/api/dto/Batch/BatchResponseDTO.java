d37c26f4437b19554012babe06a625ab8d5e61ef
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-09-25 08:25:20 -0300

    Subindo batch para swagger

--- src/main/java/com/api/dto/Batch/BatchResponseDTO.java
+++ src/main/java/com/api/dto/Batch/BatchResponseDTO.java
@@ -2,16 +2,25 @@ package com.api.dto.Batch;
 
 import com.api.Model.Batch;
 import com.api.dto.address.AddressResponseDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
 public class BatchResponseDTO {
+    @Schema(description = "ID do lote", example = "1")
     private long id;
+    @Schema(description = "Unidade de medida do lote", example = "kg, g, l, ml, un")
     private String unit_measure;
+    @NotNull(message = "Data de entrada vazia vazio")
     private Date entry_date;
+    @Schema(description = "Código do lote", example = "ABC123")
     private String batch_code;
+    @Schema(description = "Data de validade do lote", example = "2024-10-10")
     private Date expiration_date;
+    @Schema(description = "Quantidade de itens no lote", example = "100")
     private int quantity;
+    @Schema(description = "ID do produto associado ao lote", example = "1")
     private long product_id;
 
 
