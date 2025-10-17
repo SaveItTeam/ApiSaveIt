e22ecf36cbab70557b51879fbe98408bec4bedb1
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-17 10:23:06 -0300

    Ajustes do campos

--- src/main/java/com/api/dto/showcase/ShowcaseRequestDTO.java
+++ src/main/java/com/api/dto/showcase/ShowcaseRequestDTO.java
@@ -17,15 +17,27 @@ public class ShowcaseRequestDTO {
     @Schema(description = "Descrição do produto na vitrine", example = "Produto em promoção")
     private String description;
 
-    @NotNull(message = "Preço vazio")
-    @Schema(description = "Preço do produto na vitrine", example = "99.99")
-    private double price;
-
     @NotNull(message = "lote_id vazio")
     @Schema(description = "ID do lote associado à vitrine", example = "1")
-    private long batch_id;
+    private long batchId;
+
+    @NotNull(message = "Quantidade na vitrine vazia")
+    @Schema(description = "Quantidade do produto na vitrine", example = "50")
+    private Integer quantityShowcase;
+
+    @NotNull(message = "entrance_showcase vazio")
+    @Schema(description = "Data de entrada do produto na vitrine", example = "2023-10-01T10:00:00Z")
+    private Date entranceShowcase;
+
+    @NotNull(message = "Nome vazio")
+    @Schema(description = "Nome do produto na vitrine", example = "Camiseta Promoção")
+    private String name;
 
     @NotNull(message = "Data de entrada vazia")
     @Schema(description = "Data de entrada do produto na vitrine", example = "2023-10-01T10:00:00Z")
-    private Date entrance_date;
+    private Date entranceDate;
+
+    @NotNull(message = "Preço vazio")
+    @Schema(description = "Preço do produto na vitrine", example = "99.99")
+    private Double price;
 }
