9cfd4a887332e81d87bbc82a4af13d259220a77c
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-20 08:53:29 -0300

    Image: Subindo ajustes de swagger para OpenApi

--- src/main/java/com/api/Controller/ImageController.java
+++ src/main/java/com/api/Controller/ImageController.java
@@ -1,6 +1,7 @@
 package com.api.Controller;
 
 import com.api.Exception.GlobalException;
+import com.api.OpenAPI.ImageOpenApi;
 import com.api.Service.ImageService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,7 +22,7 @@ import com.api.dto.image.ImageResponseDTO;
 
 @RestController
 @RequestMapping("/api/image")
-public class ImageController {
+public class ImageController implements ImageOpenApi {
     private final ImageService imageService;
     private GlobalException ge;
 
@@ -31,31 +32,15 @@ public class ImageController {
     }
 
     @GetMapping("/selecionar")
-    @Operation(summary = "Listar todas as imagens")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista de imagens retornada com sucesso"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
     public ResponseEntity<List<ImageResponseDTO>> listImage() {
         List<ImageResponseDTO> images = imageService.listImage();
         return ResponseEntity.ok(images);
     }
     @GetMapping("/showcase-images/{showcaseId}")
-    @Operation(summary = "Listar imagens para vitrine")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista de imagens para vitrine retornada com sucesso"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
     public ResponseEntity<?> getShowcaseWithImages(@PathVariable Long showcaseId) {
         return ResponseEntity.ok(imageService.listShowcaseWithImages(showcaseId));
     }
     @PostMapping("/inserir")
-    @Operation(summary = "Inserir uma nova imagem")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Imagem inserida com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
     public ResponseEntity<?> insertImage(@RequestBody ImageRequestDTO image) {
         imageService.insertImage(image);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -63,60 +48,28 @@ public class ImageController {
     }
 
     @GetMapping("/selecionar/{id}")
-    @Operation(summary = "Obter uma imagem por ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Imagem retornada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
     public ResponseEntity<ImageResponseDTO> getImageById(@PathVariable Long id) {
         ImageResponseDTO image = imageService.getImageById(id);
         return ResponseEntity.ok(image);
     }
 
     @GetMapping("/selecionarPorProduto/{productId}")
-    @Operation(summary = "Obter uma imagem por ID do produto")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Imagem retornada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
     public ResponseEntity<ImageResponseDTO> getImageByProductId(@PathVariable Long productId) {
         ImageResponseDTO image = imageService.getImageByProductId(productId);
         return ResponseEntity.ok(image);
     }
 
     @DeleteMapping("/excluir/{id}")
-    @Operation(summary = "Excluir uma imagem")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Imagem excluída com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
     public ResponseEntity<?> deleteImage(@PathVariable Long id) {
         imageService.deleteImage(id);
         return ResponseEntity.ok("Imagem excluído com sucesso!");
     }
     @PutMapping("/atualizar/{id}")
-    @Operation(summary = "Atualizar uma imagem")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Imagem atualizada com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos"),
-            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
     public ResponseEntity<?> updateImage(@PathVariable Long id, @Valid @RequestBody ImageRequestDTO imageAtualizado) {
         imageService.updateImage(id, imageAtualizado);
         return ResponseEntity.ok("Imagem atualizada com sucesso!");
     }
     @PatchMapping("/atualizarParcial/{id}")
-    @Operation(summary = "Atualizar parcialmente uma imagem")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Imagem atualizada com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos"),
-            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
     public ResponseEntity<?> updateImagePartial(@PathVariable Long id, @Valid @RequestBody Map<String, Object> updates) {
         imageService.updateImagePartial(id, updates);
         return ResponseEntity.ok("Imagem atualizado parcialmente com sucesso!");
---
+++ src/main/java/com/api/OpenAPI/ImageOpenApi.java
@@ -0,0 +1,90 @@
+package com.api.OpenAPI;
+
+import com.api.dto.image.ImageRequestDTO;
+import com.api.dto.image.ImageResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ImageOpenApi {
+
+    @Operation(summary = "Listar todas as imagens")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de imagens retornada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    ResponseEntity<List<ImageResponseDTO>> listImage();
+
+
+    @Operation(summary = "Listar imagens para vitrine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de imagens para vitrine retornada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    ResponseEntity<?> getShowcaseWithImages(Long showcaseId);
+
+
+    @Operation(summary = "Inserir uma nova imagem")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Imagem inserida com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    ResponseEntity<?> insertImage(ImageRequestDTO image);
+
+
+    @Operation(summary = "Obter uma imagem por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Imagem retornada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    ResponseEntity<ImageResponseDTO> getImageById(Long id);
+
+
+    @Operation(summary = "Obter uma imagem por ID do produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Imagem retornada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+     ResponseEntity<ImageResponseDTO> getImageByProductId(Long productId);
+
+
+    @Operation(summary = "Excluir uma imagem")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Imagem excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    ResponseEntity<?> deleteImage(Long id);
+
+
+    @Operation(summary = "Atualizar uma imagem")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Imagem atualizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos"),
+            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    ResponseEntity<?> updateImage(Long id, ImageRequestDTO imageAtualizado);
+
+
+    @Operation(summary = "Atualizar parcialmente uma imagem")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Imagem atualizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos"),
+            @ApiResponse(responseCode = "404", description = "Imagem não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    ResponseEntity<?> updateImagePartial(Long id, Map<String, Object> updates);
+}
+
+
