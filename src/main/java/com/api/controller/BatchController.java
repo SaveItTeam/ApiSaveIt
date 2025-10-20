e1f160dbf20225ee71d652a10ac0825552b912c2
carvalho-joao <joao.carvalho@germinare.org.br>
2025-10-20 12:13:15 -0300

    alterando coisas no controller

--- src/main/java/com/api/controller/BatchController.java
+++ src/main/java/com/api/controller/BatchController.java
@@ -1,4 +1,4 @@
-package com.api.controller;
+package com.api.Controller;
 
 import com.api.exception.GlobalException;
 import com.api.openapi.BatchOpenApi;
@@ -48,7 +48,7 @@ public class BatchController implements BatchOpenApi {
     }
 
     @PostMapping("/inserir")
-    public ResponseEntity<?> insertBatch(@RequestBody BatchInsertRequestDTO batch) {
+    public ResponseEntity<?> insertBatch(@Valid @RequestBody BatchInsertRequestDTO batch) {
         ProductResponseDTO productResponse = productService.insertProduct(batch.getProduct());
 
         BatchRequestDTO batchRequestDTO = batch.getBatch();
@@ -67,9 +67,9 @@ public class BatchController implements BatchOpenApi {
         return ResponseEntity.ok(batchService.listProductBatch(enterpriseId));
     }
 
-    @GetMapping("informacoesProduto/{batchId}")
-    public ResponseEntity<ProductResponseInfoDTO> getProductInfo(@RequestParam Long id) {
-        BatchResponseDTO batch = batchService.getBatchById(id);
+    @GetMapping("/informacoesProduto/{idBatch}")
+    public ResponseEntity<ProductResponseInfoDTO> getProductInfo(@PathVariable Long idBatch) {
+        BatchResponseDTO batch = batchService.getBatchById(idBatch);
         ProductResponseDTO product = productService.getProductById(batch.getProductId());
         ImageResponseDTO image = imageService.getImageByProductId(product.getId());
         ProductResponseInfoDTO productInfo = new ProductResponseInfoDTO(product, batch, image);
