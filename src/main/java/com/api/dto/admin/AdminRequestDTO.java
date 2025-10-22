a713c176bed17f46c0f59d198b52b8c7dac2be0b
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-22 11:30:15 -0300

    Criação do request/response

---
+++ src/main/java/com/api/dto/admin/AdminRequestDTO.java
@@ -0,0 +1,18 @@
+package com.api.dto.admin;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class AdminRequestDTO {
+    @Column(length = 255)
+    @Schema(description = "Email de usuário do administrador", example = "adminuser@gmail.com")
+    private String email;
+    @Column(length = 255)
+    @Schema(description = "Nome completo do administrador", example = "Administrador Exemplo")
+    private String name;
+    @Column(length = 255)
+    @Schema(description = "Senha do administrador", example = "senhaSegura123")
+    private String password;
+}
---
+++ src/main/java/com/api/dto/admin/AdminResponseDTO.java
@@ -0,0 +1,16 @@
+package com.api.dto.admin;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Value;
+
+@Value
+public class AdminResponseDTO {
+    @Schema(description = "ID do administrador", example = "1")
+    private Long id;
+    @Schema(description = "Email de usuário do administrador", example = "adminuser@gmail.com")
+    private String email;
+    @Schema(description = "Nome completo do administrador", example = "Administrador Exemplo")
+    private String name;
+    @Schema(description = "Senha do administrador", example = "senhaSegura123")
+    private String password;
+}
