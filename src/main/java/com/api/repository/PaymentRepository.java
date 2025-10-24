8c47fed16a56a694ad2a0ad7fef4e1e4413a4fcf
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-24 17:21:34 -0300

    Criação de query em payment

--- src/main/java/com/api/repository/PaymentRepository.java
+++ src/main/java/com/api/repository/PaymentRepository.java
@@ -1,7 +1,29 @@
 package com.api.repository;
 
+import com.api.dto.payments.PaymentStatusResponseDTO;
 import com.api.model.Payments;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface PaymentRepository extends JpaRepository<Payments, Long> {
+
+    @Query("""
+        SELECT new com.api.dto.payments.PaymentStatusResponseDTO(
+            e.id,
+            e.name,
+            p.name,
+            pay.paymentDate,
+            pay.amount,
+            pay.status,
+            pay.paymentMethod
+        )
+        FROM Payments pay
+        JOIN pay.enterprise e
+        JOIN pay.plan p
+        ORDER BY pay.paymentDate DESC
+    """)
+    List<PaymentStatusResponseDTO> findAllPaymentsWithEnterpriseAndPlan();
 }
