15e43f3e668d3c9b4b2e3918245d2e74d72d1e2b
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-26 18:22:30 -0300

    Ajuste para swagger

--- src/main/java/com/api/openapi/OpenApiConfig.java
+++ src/main/java/com/api/openapi/OpenApiConfig.java
@@ -2,6 +2,8 @@ package com.api.openapi;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,20 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("API SaveIt")
-                        .description("Documentação da API com autenticação por sessão (form login)")
-                        .version("1.0"));
+                        .description("Documentação da API com autenticação por token ou login")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
