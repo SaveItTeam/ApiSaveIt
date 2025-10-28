62b3958d587efde4f597d5de3e4ff76e82f77f6a
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-28 11:02:48 -0300

    Ajustando o applicationTests

--- src/test/java/com/api/ApiApplicationTests.java
+++ src/test/java/com/api/ApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class ApiApplicationTests {
 
     @Test
     void contextLoads() {
+        // Apenas valida se o contexto carrega com H2
     }
-
 }
