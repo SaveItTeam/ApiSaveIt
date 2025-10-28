df9e9126cf28494ea4ff92778b59c89086ffbf94
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-28 11:46:43 -0300

    Criação de ApiApplicationTests

--- src/test/java/com/api/ApiApplicationTests.java
+++ src/test/java/com/api/ApiApplicationTests.java
@@ -1,14 +1,16 @@
 package com.api;
 
+import com.api.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest
+@Import(TestSecurityConfig.class)
 class ApiApplicationTests {
+
     @Test
     void contextLoads() {
-        // Apenas valida se o contexto carrega com H2
+        // Só verifica se o contexto carrega corretamente
     }
 }
-
