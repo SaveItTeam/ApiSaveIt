12a5301c302acc130407ee4e845e02320cb0060a
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-28 11:33:40 -0300

    Ajuste de Application

--- src/test/java/com/api/ApiApplicationTests.java
+++ src/test/java/com/api/ApiApplicationTests.java
@@ -5,10 +5,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
-@Import(SecurityTestConfig.class)
 class ApiApplicationTests {
     @Test
     void contextLoads() {
         // Apenas valida se o contexto carrega com H2
     }
 }
+
