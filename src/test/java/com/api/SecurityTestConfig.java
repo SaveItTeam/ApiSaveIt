37ea03959ee91fdadd794212fbfb0d50b6ff5c10
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-28 11:34:00 -0300

    Ajuste de testconfig

--- src/test/java/com/api/SecurityTestConfig.java
+++ src/test/java/com/api/SecurityTestConfig.java
@@ -3,16 +3,15 @@ package com.api;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @TestConfiguration
+@Profile("test")
 public class SecurityTestConfig {
-
     @Bean
-    @ConditionalOnMissingBean(AuthenticationManager.class)
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
 }
