123f74df74370e51f6936f3a69f88e8926fe0dcf
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-28 11:25:54 -0300

    Ajuste pipeline

--- src/test/java/com/api/SecurityTestConfig.java
+++ src/test/java/com/api/SecurityTestConfig.java
@@ -1,14 +1,18 @@
 package com.api;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @TestConfiguration
 public class SecurityTestConfig {
 
     @Bean
-    public AuthenticationManager authenticationManager() {
-        return authentication -> authentication; // simples mock
+    @ConditionalOnMissingBean(AuthenticationManager.class)
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
+
 }
