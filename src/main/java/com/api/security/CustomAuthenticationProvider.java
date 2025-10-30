c1a6109713340750a53d62e1ab3083015766e97c
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-10-30 10:41:44 -0300

    Autenticação Customizada

---
+++ src/main/java/com/api/security/CustomAuthenticationProvider.java
@@ -0,0 +1,44 @@
+package com.api.security;
+
+import com.api.Util.PasswordUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public CustomAuthenticationProvider(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String rawPassword = authentication.getCredentials().toString();
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        boolean senhaCorreta = PasswordUtils.verifyPassword(rawPassword, userDetails.getPassword());
+        if (!senhaCorreta) {
+            throw new BadCredentialsException("Credenciais inválidas");
+        }
+
+        return new UsernamePasswordAuthenticationToken(
+                userDetails.getUsername(),
+                userDetails.getPassword(),
+                userDetails.getAuthorities()
+        );
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
