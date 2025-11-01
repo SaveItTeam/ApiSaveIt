671436d4754c88411fb3dbf685f7dceb3dd9c410
NathanFGerminare <nathan.barreto@germinare.org.br>
2025-11-01 18:46:56 -0300

    Adicionando schema nos DTOS

--- src/main/java/com/api/dto/product/ProductRequestDTO.java
+++ src/main/java/com/api/dto/product/ProductRequestDTO.java
@@ -1,5 +1,6 @@
 package com.api.dto.product;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -8,15 +9,20 @@ import lombok.Data;
 public class ProductRequestDTO {
     @NotNull(message = "Nome vazio")
     @Column(length = 100)
+    @Schema(description = "Nome do produto", example = "Camiseta")
     private String name;
     @NotNull(message = "Marca vazio")
     @Column(length = 50)
+    @Schema(description = "Marca do produto", example = "Nike")
     private String brand;
     @Column(length = 50)
     @NotNull(message = "Categoria vazia")
+    @Schema(description = "Categoria do produto", example = "Roupas")
     private String category;
     @Column(length = 500)
+    @Schema(description = "Descrição do produto", example = "Camiseta 100% algodão")
     private String description;
+    @Schema(description = "ID da empresa do produto", example = "1")
     @NotNull(message = "Id da empresa vazia")
     private Long enterpriseId;
 }
--- src/main/java/com/api/dto/product/ProductResponseDTO.java
+++ src/main/java/com/api/dto/product/ProductResponseDTO.java
@@ -1,13 +1,20 @@
 package com.api.dto.product;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 
 @Value
 public class ProductResponseDTO {
+    @Schema(description = "ID do produto", example = "1")
     private long id;
+    @Schema(description = "Nome do produto", example = "Camiseta")
     private String name;
+    @Schema(description = "Preço do produto", example = "49.90")
     private String brand;
+    @Schema(description = "Categoria do produto", example = "Roupas")
     private String category;
+    @Schema(description = "Descrição do produto", example = "Camiseta 100% algodão")
     private String description;
+    @Schema(description = "ID da empresa do produto", example = "1")
     private long enterpriseId;
 }
